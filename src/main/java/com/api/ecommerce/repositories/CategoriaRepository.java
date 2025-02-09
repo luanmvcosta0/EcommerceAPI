cceaa501be532daf4f43c83e8ed7d4660c6f3274
luanmvcosta0 <luanmvcosta@mgail.com>
2025-02-09 11:04:36 -0300

    Criando a interface CategoriaRepository

---
+++ src/main/java/com/api/ecommerce/repositories/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.api.ecommerce.repositories;
+
+import com.api.ecommerce.models.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+}
