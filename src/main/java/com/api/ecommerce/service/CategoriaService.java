17b241cc3ee82c03a2455ad938f4a91df0c600d4
luanmvcosta0 <luanmvcosta@gmail.com>
2025-02-16 17:01:56 -0300

    Criando o service da entidade Categoria

---
+++ src/main/java/com/api/ecommerce/service/CategoriaService.java
@@ -0,0 +1,13 @@
+package com.api.ecommerce.service;
+
+import com.api.ecommerce.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+}
