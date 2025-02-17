942e9fcb5481770accbc62bdc03a8ffa18c34c44
luanmvcosta0 <luanmvcosta@mgail.com>
2025-02-17 20:23:23 -0300

    Adicionando GlobalException e tratando a execao do findById

---
+++ src/main/java/com/api/ecommerce/exceptions/GlobalException.java
@@ -0,0 +1,20 @@
+package com.api.ecommerce.exceptions;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class GlobalException {
+
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<StandardError> objectNotFoundException(ObjectNotFoundException ex, HttpServletRequest request) {
+        StandardError error = new StandardError(LocalDateTime.now(), HttpStatus.NOT_FOUND.value(), ex.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+}
