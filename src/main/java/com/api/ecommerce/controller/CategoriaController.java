6128f386b24e244bcc0dbbb1e2615e3200fbf39d
luanmvcosta0 <luanmvcosta@mgail.com>
2025-02-17 20:12:28 -0300

    Criando o endpoint findById

--- src/main/java/com/api/ecommerce/controller/CategoriaController.java
+++ src/main/java/com/api/ecommerce/controller/CategoriaController.java
@@ -1,9 +1,12 @@
 package com.api.ecommerce.controller;
 
+import com.api.ecommerce.dtos.CategoriaDto;
 import com.api.ecommerce.models.Categoria;
 import com.api.ecommerce.service.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +22,12 @@ public class CategoriaController {
     @GetMapping
     public List<Categoria> findAll() {
         return categoriaService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoriaDto> findById(@PathVariable Long id) {
+        Categoria cat = categoriaService.findById(id);
+        return ResponseEntity.ok().body(new CategoriaDto(cat));
     }
 
 }
--- src/main/java/com/api/ecommerce/service/CategoriaService.java
+++ src/main/java/com/api/ecommerce/service/CategoriaService.java
@@ -1,11 +1,13 @@
 package com.api.ecommerce.service;
 
+import com.api.ecommerce.exceptions.ObjectNotFoundException;
 import com.api.ecommerce.models.Categoria;
 import com.api.ecommerce.repositories.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoriaService {
@@ -15,6 +17,14 @@ public class CategoriaService {
 
     public List<Categoria> findAll() {
         return categoriaRepository.findAll();
+    }
+
+    public Categoria findById(Long id) {
+        Optional<Categoria> cat = categoriaRepository.findById(id);
+        if (cat.isPresent()) {
+            return cat.get();
+        }
+        throw new ObjectNotFoundException("Categoria não encontrada");
     }
 
 }
