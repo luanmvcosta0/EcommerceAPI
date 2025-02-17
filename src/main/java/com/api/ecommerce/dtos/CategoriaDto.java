e882d843843d69e3e7817814c7068315f09de138
luanmvcosta0 <luanmvcosta@gmail.com>
2025-02-16 21:57:11 -0300

    Criando o DTO da minha entidade categoria

---
+++ src/main/java/com/api/ecommerce/dtos/CategoriaDto.java
@@ -0,0 +1,53 @@
+package com.api.ecommerce.dtos;
+
+import com.api.ecommerce.models.Categoria;
+
+public class CategoriaDto {
+
+    private Long id;
+    private String nome;
+    private String descricao;
+
+
+
+    public CategoriaDto() {;
+    }
+
+    public CategoriaDto(Long id, String nome, String descricao) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+    public CategoriaDto(Categoria categoria) {
+        this.id = categoria.getId();
+        this.nome = categoria.getNome();
+        this.descricao = categoria.getDescricao();
+    }
+
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
