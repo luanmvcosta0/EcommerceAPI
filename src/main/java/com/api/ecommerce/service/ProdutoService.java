3baa69b79019550c8b4e9e7ab7ac93f0abe4324d
luanmvcosta0 <luanmvcosta@gmail.com>
2025-03-05 16:42:09 -0300

    Corrigindo a injecao de dependencia do ProdutoService

--- src/main/java/com/api/ecommerce/service/ProdutoService.java
+++ src/main/java/com/api/ecommerce/service/ProdutoService.java
@@ -3,6 +3,7 @@ package com.api.ecommerce.service;
 import com.api.ecommerce.dtos.ProdutoDto;
 import com.api.ecommerce.exceptions.ObjectNotFoundException;
 import com.api.ecommerce.models.Produto;
+import com.api.ecommerce.repositories.CategoriaRepository;
 import com.api.ecommerce.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,14 +17,14 @@ public class ProdutoService {
     private ProdutoRepository produtoRepository;
 
     @Autowired
-    private CategoriaService categoriaService;
+    private CategoriaRepository categoriaRepository;
 
     public List<Produto> findAll() {
         return produtoRepository.findAll();
     }
 
     public List<ProdutoDto> findAllByCategoria(Long idCategoria) {
-        categoriaService.findById(idCategoria);
+        categoriaRepository.findById(idCategoria);
 
         List<Produto> produtos = produtoRepository.findByCategoriaId(idCategoria);
 
