6168b9e43c70f88e32e698c5b422d9561fdd859b
luanmvcosta0 <luanmvcosta@gmail.com>
2025-03-05 08:48:03 -0300

    Criando o ProdutoService e corrigindo o nome da classe ProdutoRepository

---
+++ src/main/java/com/api/ecommerce/service/ProdutoService.java
@@ -0,0 +1,13 @@
+package com.api.ecommerce.service;
+
+import com.api.ecommerce.repositories.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProdutoService {
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+}
