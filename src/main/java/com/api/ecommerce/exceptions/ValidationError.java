247c71d918a749a8bf4672162d3ec1b4a6884f4e
luanmvcosta0 <luanmvcosta@gmail.com>
2025-03-04 21:39:52 -0300

    Criando a classe ValidationError orpara ganiza ors erros de validação de forma clara e estruturada

---
+++ src/main/java/com/api/ecommerce/exceptions/ValidationError.java
@@ -0,0 +1,25 @@
+package com.api.ecommerce.exceptions;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError {
+
+    private List<FieldMessage> erros = new ArrayList<>();
+
+    public ValidationError() {
+    }
+
+    public ValidationError(LocalDateTime timestamp, Integer status, String message, String path) {
+        super(timestamp, status, message, path);
+    }
+
+    public ValidationError(List<FieldMessage> erros) {
+        this.erros = erros;
+    }
+
+    public void addErro(String fieldName, String message) {
+        this.erros.add(new FieldMessage(fieldName, message));
+    }
+}
