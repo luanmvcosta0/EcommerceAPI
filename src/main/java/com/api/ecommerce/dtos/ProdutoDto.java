300bedc40fe1dfd5921d71ad869ffbc08f6dd5e5
luanmvcosta0 <luanmvcosta@gmail.com>
2025-03-05 08:43:01 -0300

    Criando o DTO da entidade Produto

---
+++ src/main/java/com/api/ecommerce/dtos/ProdutoDto.java
@@ -0,0 +1,73 @@
+package com.api.ecommerce.dtos;
+
+import com.api.ecommerce.models.Categoria;
+
+public class ProdutoDto {
+
+    private Long id;
+    private String nome;
+    private String descricao;
+    private Double preco;
+    private Integer quantidadeEstoque;
+    private Categoria categoria;
+
+    public ProdutoDto() {
+    }
+
+    public ProdutoDto(Long id, String nome, String descricao, Double preco, Integer quantidadeEstoque, Categoria categoria) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.quantidadeEstoque = quantidadeEstoque;
+        this.categoria = categoria;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public Integer getQuantidadeEstoque() {
+        return quantidadeEstoque;
+    }
+
+    public void setQuantidadeEstoque(Integer quantidadeEstoque) {
+        this.quantidadeEstoque = quantidadeEstoque;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+}
