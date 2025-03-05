3701c90bf7ac1108d667b784a0e4056bb10d406d
luanmvcosta0 <luanmvcosta@gmail.com>
2025-03-05 09:28:24 -0300

    Criando o endpoint findById do Produto

--- src/main/java/com/api/ecommerce/controller/ProdutoController.java
+++ src/main/java/com/api/ecommerce/controller/ProdutoController.java
@@ -1,14 +1,13 @@
 package com.api.ecommerce.controller;
 
 import com.api.ecommerce.dtos.ProdutoDto;
-import com.api.ecommerce.repositories.ProdutoRepository;
 import com.api.ecommerce.service.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 
 @RestController
@@ -21,6 +20,11 @@ public class ProdutoController {
     @GetMapping
     public ResponseEntity<List<ProdutoDto>> findAll() {
         return ResponseEntity.ok(produtoService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProdutoDto> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(produtoService.findById(id));
     }
 
 }
--- src/main/java/com/api/ecommerce/service/ProdutoService.java
+++ src/main/java/com/api/ecommerce/service/ProdutoService.java
@@ -1,6 +1,8 @@
 package com.api.ecommerce.service;
 
 import com.api.ecommerce.dtos.ProdutoDto;
+import com.api.ecommerce.exceptions.ObjectNotFoundException;
+import com.api.ecommerce.models.Produto;
 import com.api.ecommerce.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,6 +20,13 @@ public class ProdutoService {
                 .stream()
                 .map(ProdutoDto::new)
                 .collect(Collectors.toList());
+    }
+
+    public ProdutoDto findById(Long id) {
+        Produto produto = produtoRepository.findById(id)
+                .orElseThrow(() -> new ObjectNotFoundException("Produto não encontrado"));
+
+        return new ProdutoDto(produto);
     }
 
 }
