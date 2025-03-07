7d047633e25a48abe4f809baaae21154a8ea9cbb
luanmvcosta0 <luanmvcosta@gmail.com>
2025-03-07 14:48:16 -0300

    Finalizando projeto ecommerceApi

--- src/main/java/com/api/ecommerce/controller/CategoriaController.java
+++ src/main/java/com/api/ecommerce/controller/CategoriaController.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/categoria")
+@CrossOrigin("*")
 public class CategoriaController {
 
     @Autowired
--- src/main/java/com/api/ecommerce/controller/ProdutoController.java
+++ src/main/java/com/api/ecommerce/controller/ProdutoController.java
@@ -3,9 +3,11 @@ package com.api.ecommerce.controller;
 import com.api.ecommerce.dtos.ProdutoDto;
 import com.api.ecommerce.models.Produto;
 import com.api.ecommerce.service.ProdutoService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,11 +21,13 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/produto")
+@CrossOrigin("*")
 public class ProdutoController {
 
     @Autowired
     private ProdutoService produtoService;
 
+    @Operation(summary = "Busca toodos os produtos existentes")
     @GetMapping("")
     public ResponseEntity<List<ProdutoDto>> findAll() {
         List<Produto> produtos = produtoService.findAll();
@@ -34,30 +38,35 @@ public class ProdutoController {
         return ResponseEntity.ok(produtosDto);
     }
 
+    @Operation(summary = "Busca todos os produtos de uma categoria")
     @GetMapping("/categoria/{id}")
     public ResponseEntity<List<ProdutoDto>> findAllByCategoria(@PathVariable Long id) {
         List<ProdutoDto> produtos = produtoService.findAllByCategoria(id);
         return ResponseEntity.ok(produtos);
     }
 
+    @Operation(summary = "Busca o produto pelo ID")
     @GetMapping("{id}")
     public ResponseEntity<ProdutoDto> findById(@PathVariable Long id) {
         Produto produto = produtoService.findById(id);
         return ResponseEntity.ok(new ProdutoDto(produto));
     }
 
+    @Operation(summary = "Cadastra um novo produto")
     @PostMapping("")
     public ResponseEntity<ProdutoDto> save(@Valid @RequestBody ProdutoDto produtoDto){
         Produto produtoSalvo = produtoService.save(produtoDto);
         return ResponseEntity.ok(new ProdutoDto(produtoSalvo));
     }
 
+    @Operation(summary = "Atualiza o produto")
     @PutMapping("{id}")
     public ResponseEntity<ProdutoDto> update(@PathVariable Long id, @Valid @RequestBody ProdutoDto produtoDto) {
         Produto produtoAtualizado = produtoService.update(id, produtoDto);
         return ResponseEntity.ok(new ProdutoDto(produtoAtualizado));
     }
 
+    @Operation(summary = "Deleta o produto")
     @DeleteMapping("{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         produtoService.delete(id);
