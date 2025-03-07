4dd00cc94bbf0ce8d7ebd4a5deeffa53382e2964
luanmvcosta0 <luanmvcosta@gmail.com>
2025-03-07 12:07:31 -0300

    Adicionando dependencia para documentacao

--- src/main/java/com/api/ecommerce/controller/CategoriaController.java
+++ src/main/java/com/api/ecommerce/controller/CategoriaController.java
@@ -6,6 +6,7 @@ import com.api.ecommerce.service.CategoriaService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jmx.export.annotation.ManagedOperation;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
