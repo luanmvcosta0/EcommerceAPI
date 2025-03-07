102a72a28cedffb22f80a5c037b6a366a1a3c5b5
luanmvcosta0 <luanmvcosta@gmail.com>
2025-03-07 13:23:08 -0300

    Documentando o CategoriaController

--- src/main/java/com/api/ecommerce/controller/CategoriaController.java
+++ src/main/java/com/api/ecommerce/controller/CategoriaController.java
@@ -3,10 +3,10 @@ package com.api.ecommerce.controller;
 import com.api.ecommerce.dtos.CategoriaDto;
 import com.api.ecommerce.models.Categoria;
 import com.api.ecommerce.service.CategoriaService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.jmx.export.annotation.ManagedOperation;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +25,7 @@ public class CategoriaController {
     @Autowired
     private CategoriaService categoriaService;
 
+    @Operation(summary = "Busca todas as categorias exixtentes")
     @GetMapping("")
     public ResponseEntity<List<CategoriaDto>> findAll() {
         List<Categoria> categorias = categoriaService.findAll();
@@ -34,12 +35,14 @@ public class CategoriaController {
         return ResponseEntity.ok(categoriaDtos);
     }
 
+    @Operation(summary = "Busca a categoria pelo ID")
     @GetMapping("/{id}")
     public ResponseEntity<CategoriaDto> findById(@PathVariable Long id) {
         Categoria categoria = categoriaService.findById(id);
         return ResponseEntity.ok(new CategoriaDto(categoria));
     }
 
+    @Operation(summary = "Cadastra uma nova categoria")
     @PostMapping("")
     public ResponseEntity<CategoriaDto> save(@Valid @RequestBody CategoriaDto categoriaDto) {
         Categoria categoria = new Categoria(categoriaDto);
@@ -47,6 +50,7 @@ public class CategoriaController {
         return ResponseEntity.ok(new CategoriaDto(savedCategoria));
     }
 
+    @Operation(summary = "Atualiza a categoria")
     @PutMapping("/{id}")
     public ResponseEntity<CategoriaDto> update(@PathVariable Long id, @Valid @RequestBody CategoriaDto categoriaDto) {
         Categoria categoria = new Categoria(categoriaDto);
@@ -54,10 +58,10 @@ public class CategoriaController {
         return ResponseEntity.ok(new CategoriaDto(updatedCategoria));
     }
 
+    @Operation(summary = "Deleta a categoria")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         categoriaService.delete(id);
         return ResponseEntity.noContent().build();
     }
-
 }
