debdeabfee0bd58c443bd116d69549c80c4c6afe
luanmvcosta0 <luanmvcosta@gmail.com>
2025-03-30 19:44:58 -0300

    Criando o teste unitario Controller da entidade Categoria, metodo findAll

---
+++ src/test/java/com/api/ecommerce/controller/CategoriaControllerTest.java
@@ -0,0 +1,75 @@
+package com.api.ecommerce.controller;
+
+import com.api.ecommerce.dtos.CategoriaDto;
+import com.api.ecommerce.models.Categoria;
+import com.api.ecommerce.service.CategoriaService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(SpringExtension.class)
+class CategoriaControllerTest {
+
+    @Mock
+    private CategoriaService categoriaService;
+
+    @InjectMocks
+    private CategoriaController categoriaController;
+
+    private Categoria categoria;
+    private CategoriaDto categoriaDto;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+
+        categoria = new Categoria();
+        categoria.setId(1L);
+        categoria.setNome("Eletrônicos");
+        categoria.setDescricao("Produtos eletrônicos em geral");
+
+        categoriaDto = new CategoriaDto(categoria);
+    }
+
+    @Test
+    void Dado_categorias_existentes_Quando_buscar_todas_Entao_deve_retornar_uma_lista_de_categoriaDto() {
+        when(categoriaService.findAll()).thenReturn(Arrays.asList(categoria));
+
+        ResponseEntity<List<CategoriaDto>> listaCategoria = categoriaController.findAll();
+
+        assertNotNull(listaCategoria);
+        assertEquals(HttpStatus.OK, listaCategoria.getStatusCode());
+        assertEquals(1, listaCategoria.getBody().size());
+        assertEquals("Eletrônicos", listaCategoria.getBody().get(0).getNome());
+
+        verify(categoriaService, times(1)).findAll();
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
